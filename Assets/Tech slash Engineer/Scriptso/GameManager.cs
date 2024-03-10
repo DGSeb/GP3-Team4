e9c674e933ab80d626c61bedf8a0c9966c64b951
Dormio-san <liam.lovett06@gmail.com>
2024-03-09 19:36:03 -0500

    Pause menu and PB UI.
    Pause menu that shows up when esc key is pressed. Can resume, go to main menu, and quit. (Main Menu still needs created.) Pause menu look will be adjusted.
    PB shows the player's PB when they get a new one. If they hit the exit (temp gray cube) it updates if their time is better.

--- Assets/Tech slash Engineer/Scriptso/GameManager.cs
+++ Assets/Tech slash Engineer/Scriptso/GameManager.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
 
-public class Timer : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Components that display on UI.
-    [Header("Component")]
+    [Header("UI Components")]
     public TextMeshProUGUI timerText;
     public GameObject timeLimitReachedText;
+    public TextMeshProUGUI pBText;
+    [SerializeField] private GameObject pauseMenuUI;
 
     // Settings that control time.
     [Header("Timer Settings")]
@@ -27,6 +30,18 @@ public class Timer : MonoBehaviour
     public TimerFormats format;
     private Dictionary<TimerFormats, string> timeFormats = new Dictionary<TimerFormats, string>();
 
+    // Enum for the different formats of the time, such as 1 second or 1.1 seconds or 1.11 seconds or 1.11 seconds.
+    public enum TimerFormats
+    {
+        Whole,
+        TenthDecimal,
+        HundrethsDecimal,
+        ThousandthsDecimal
+    }
+
+    // Pause menu bool.
+    public static bool gameIsPaused;
+
     void Start()
     {
         // Add entries to the dictionary for the timer format.
@@ -34,6 +49,11 @@ public class Timer : MonoBehaviour
         timeFormats.Add(TimerFormats.TenthDecimal, "0.0");
         timeFormats.Add(TimerFormats.HundrethsDecimal, "0.00");
         timeFormats.Add(TimerFormats.ThousandthsDecimal, "0.000");
+
+        UpdatePBText();
+
+        // Make sure the pause menu is off.
+        pauseMenuUI.SetActive(false);
     }
 
     void Update()
@@ -56,6 +76,19 @@ public class Timer : MonoBehaviour
         }
 
         SetTimerText();
+
+        // Pause menu check
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
     }
 
     // Function to set the text of the timer.
@@ -67,7 +100,7 @@ public class Timer : MonoBehaviour
     }
 
     // Function to change the scene based on the name inputted when function is called.
-    void ChangeScene(string sceneName)
+    public void ChangeScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
@@ -77,18 +110,55 @@ public class Timer : MonoBehaviour
     {
         // Turn on text telling player the time limit is reached, stop time, wait about a second, reload the scene, and resume time.
         timeLimitReachedText.SetActive(true);
-        Time.timeScale = 0.0f;
+        Time.timeScale = 0f;
         yield return new WaitForSecondsRealtime(1.2f);
         ChangeScene("LiamsWackyWonderland");
         Time.timeScale = 1.0f;
+        UpdatePBText();
     }
-}
 
-// Enum for the different formats of the time, such as 1 second or 1.1 seconds or 1.11 seconds or 1.11 seconds.
-public enum TimerFormats
-{
-    Whole,
-    TenthDecimal,
-    HundrethsDecimal,
-    ThousandthsDecimal
+    // Function to check the best time the player achieved.
+    public void CheckPB()
+    {
+        if (currentTime < PlayerPrefs.GetFloat("PB", timerLimit))
+        {
+            PlayerPrefs.SetFloat("PB", currentTime);
+            UpdatePBText();
+        }
+    }
+
+    void UpdatePBText()
+    {
+        pBText.text = hasFormat ? $"PB: {PlayerPrefs.GetFloat("PB", timerLimit).ToString(timeFormats[format])}" : $"PB: {PlayerPrefs.GetFloat("PB", timerLimit)}";
+    }    
+
+    // Pause menu things
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1.0f;
+        Debug.Log("Loading menu...");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
 }
