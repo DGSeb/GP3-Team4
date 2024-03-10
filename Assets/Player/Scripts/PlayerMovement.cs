808c509803d00bad1229e1b3734a5a2275e84128
DGSeb <se437770@ucf.edu>
2024-03-10 11:28:35 -0400

    Revert "Squashed commit of the following:"
    This reverts commit 696386e8ea0de2fba5e3bd3686b98075b0ae5814.

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -9,12 +9,7 @@ public class PlayerMovement : MonoBehaviour
     public float walkSpeed;
     public float sprintSpeed;
 
-    public float dashSpeed;
-    public float dashSpeedChangeFactor;
-
     public float groundDrag;
-
-    public float maxYSpeed;
 
     [Header("Jumping")]
     public float jumpForce;
@@ -34,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
-    public bool grounded;
+    bool grounded;
 
     [Header("Keybinds")]
     public KeyCode jumpKey = KeyCode.Space;
@@ -47,8 +42,8 @@ public class PlayerMovement : MonoBehaviour
     private bool exitingSlope;
 
 
-    public float horizontalInput;
-    public float verticalInput;
+    float horizontalInput;
+    float verticalInput;
 
     Vector3 moveDirection;
 
@@ -61,11 +56,8 @@ public class PlayerMovement : MonoBehaviour
         walking,
         sprinting,
         crouching,
-        dashing,
         air
     }
-
-    public bool dashing;
 
     private void Start()
     {
@@ -80,73 +72,35 @@ public class PlayerMovement : MonoBehaviour
         MovePlayer();
     }
 
-    private float desiredMoveSpeed;
-    private float lastDesiredMoveSpeed;
-    private MovementState lastState;
-    private bool keepMomentum;
-
     private void StateHandler()
     {
 
-        // Mode - Dashing
-        if (dashing)
-        {
-            state = MovementState.dashing;
-            desiredMoveSpeed = dashSpeed;
-            speedChangeFactor = dashSpeedChangeFactor;
-        }
-
         // Mode - Crouching
-        else if (Input.GetKey(crouchKey))
+        if (Input.GetKey(crouchKey))
         {
             state = MovementState.crouching;
-            desiredMoveSpeed = crouchSpeed;
+            moveSpeed = crouchSpeed;
         }
 
         // Mode - Sprinting
         else if (grounded && Input.GetKey(sprintKey))
         {
             state = MovementState.sprinting;
-            desiredMoveSpeed = sprintSpeed;
+            moveSpeed = sprintSpeed;
         }
 
         // Mode - Walking
         else if (grounded)
         {
             state = MovementState.walking;
-            desiredMoveSpeed = walkSpeed;
+            moveSpeed = walkSpeed;
         }
 
         // Mode - Air
         else
         {
             state = MovementState.air;
-
-            if (desiredMoveSpeed < sprintSpeed)
-                desiredMoveSpeed = walkSpeed;
-            else
-                desiredMoveSpeed = sprintSpeed;
         }
-
-        bool desiredMoveSpeedHasChanged = desiredMoveSpeed != lastDesiredMoveSpeed;
-        if (lastState == MovementState.dashing) keepMomentum = true;
-
-        if (desiredMoveSpeedHasChanged)
-        {
-            if (keepMomentum)
-            {
-                StopAllCoroutines();
-                StartCoroutine(SmoothlyLerpMoveSpeed());
-            }
-            else
-            {
-                StopAllCoroutines();
-                moveSpeed = desiredMoveSpeed;
-            }
-        }
-
-        lastDesiredMoveSpeed = desiredMoveSpeed;
-        lastState = state;
     }
 
     private void Update()
@@ -159,7 +113,7 @@ public class PlayerMovement : MonoBehaviour
         StateHandler();
 
         // handle drag
-        if (grounded && state != MovementState.dashing)
+        if (grounded)
             rb.drag = groundDrag;
         else
             rb.drag = 0;
@@ -179,7 +133,7 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // when to crouch
-        if (Input.GetKeyDown(crouchKey) && GameManager.isPlayerActive)
+        if (Input.GetKeyDown(crouchKey))
         {
             transform.localScale = new Vector3(transform.localScale.x, crouchYScale, transform.localScale.z);
             if (grounded)
@@ -188,7 +142,7 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyUp(crouchKey) && GameManager.isPlayerActive)
+        if (Input.GetKeyUp(crouchKey))
         {
             transform.localScale = new Vector3(transform.localScale.x, startYscale, transform.localScale.z);
         }
@@ -196,9 +150,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void MovePlayer()
     {
-        // disable when dashing to prevent gravity conflict
-        if (state == MovementState.dashing) return;
-
         // calculate movement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
@@ -252,10 +203,6 @@ public class PlayerMovement : MonoBehaviour
                 rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
             }
         }
-
-        // limit y vel
-        if (maxYSpeed != 0 && rb.velocity.y > maxYSpeed)
-            rb.velocity = new Vector3(rb.velocity.x, maxYSpeed, rb.velocity.z);
     }
 
     private void Jump()
@@ -289,29 +236,5 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 GetSlopeMoveDirection()
     {
         return Vector3.ProjectOnPlane(moveDirection, slopeHit.normal).normalized;
-    }
-
-    private float speedChangeFactor;
-    private IEnumerator SmoothlyLerpMoveSpeed()
-    {
-        // smoothly lerp movementSpeed to desired value
-        float time = 0;
-        float difference = Mathf.Abs(desiredMoveSpeed - moveSpeed);
-        float startValue = moveSpeed;
-
-        float boostFactor = speedChangeFactor;
-
-        while (time < difference)
-        {
-            moveSpeed = Mathf.Lerp(startValue, desiredMoveSpeed, time / difference);
-
-            time += Time.deltaTime * boostFactor;
-
-            yield return null;
-        }
-
-        moveSpeed = desiredMoveSpeed;
-        speedChangeFactor = 1f;
-        keepMomentum= false;    
     }
 }
--- Assets/Player/Weapon/WeaponHandler.cs
+++ Assets/Player/Weapon/WeaponHandler.cs
@@ -7,95 +7,16 @@ public class WeaponHandler : MonoBehaviour
     [Header("Keybinds")]
     public KeyCode shootKey = KeyCode.Mouse0;
 
-    [Header("References")]
+    [Header("Objects")]
     public GameObject Effect;
-    public GameObject hitEffect;
-    public GameObject trail;
     public Transform Emitter;
-    public Camera playerCam;
-    public PlayerCam recoilScript;
-
-    [Header("Options")]
-    [SerializeField] float cooldown;
-    [SerializeField] float travelSpeedFactor;
-
-    [Header("Recoil Vectors")]
-    [SerializeField] private float recoilX;
-    [SerializeField] private float recoilY;
-    [SerializeField] private float recoilZ;
-
-    private bool canFire = true;
 
     private void Update()
     {
-        if (Input.GetKey(shootKey) && canFire && GameManager.isPlayerActive)
+        if (Input.GetKeyDown(shootKey))
         {
-            canFire = false;
-            StartCoroutine(Shoot(cooldown));
-        }
-    }
-
-    private IEnumerator Shoot(float waitTime)
-    {
-        print("Pew!");
-        recoilScript.RecoilFire(recoilX, recoilY, recoilZ);
-
-        GameObject effectInstance = Instantiate(Effect, Emitter);
-        Destroy(effectInstance, 5f);
-
-        Vector3 aimPosition = playerCam.transform.position + playerCam.transform.forward * 100;
-
-        RaycastHit hit;
-        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, 9999f))
-        {
-            Debug.Log(hit.transform.name);
-
-            aimPosition = hit.point;
-
-            GameObject trailInstance = Instantiate(trail, Emitter);
-            Destroy(trailInstance, 5f);
-
-            StartCoroutine(LerpPosition(aimPosition, Vector3.Distance(Emitter.position, aimPosition) * travelSpeedFactor, trailInstance));
-
-            //Vector3.Distance(Emitter.position, aimPosition) * travelSpeedFactor
-        }
-        else
-        {
-            GameObject trailInstance = Instantiate(trail, Emitter);
-            Destroy(trailInstance, 5f);
-            StartCoroutine(LerpPosition(aimPosition, Vector3.Distance(Emitter.position, aimPosition) * travelSpeedFactor, trailInstance));
-        }
-
-        yield return new WaitForSeconds(waitTime);
-
-        canFire = true;
-    }
-
-    IEnumerator LerpPosition(Vector3 targetPosition, float duration, GameObject thingToLerp)
-    {
-        if (thingToLerp != null)
-        {
-            float time = 0;
-            Vector3 startPosition = Emitter.transform.position;
-
-            while (time < duration && thingToLerp != null)
-            {
-                thingToLerp.transform.position = Vector3.Lerp(startPosition, targetPosition, time / duration);
-                time += Time.deltaTime;
-                yield return null;
-            }
-            if (thingToLerp != null)
-            {
-                thingToLerp.transform.position = targetPosition;
-                thingToLerp.GetComponent<TrailRenderer>().enabled = false;
-
-                GameObject hitEffectInstance = Instantiate(hitEffect, thingToLerp.transform.position, thingToLerp.transform.rotation);
-                Destroy(hitEffectInstance, 5f);
-            }
-        }
-        else
-        {
-            yield return null;
+            print("Pew!");
+            Instantiate(Effect, Emitter);
         }
     }
 }
